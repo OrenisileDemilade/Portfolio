c1977435a11d2e49c0e942a99d889356cbc75685
OrenisileDemilade <dorenisile@gmail.com>
2024-02-09 00:04:42 -0600

    fifth commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// const hamburger = document.querySelector(".hamburger");
-// hamburger.onclick = function() {
-//     navBar = document.querySelector(".nav-home");
-//         navBar.classList.toggle("active");
-// }
+const hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function() {
+    navBar = document.querySelector(".nav-home");
+        navBar.classList.toggle("active");
+}
 
 const nav = document.querySelector("nav");
 window.addEventListener("scroll", function(){
